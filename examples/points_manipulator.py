3021656f64dfccda21ce2bba7d88841716d8b3c2
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-01-28 17:40:34 +0100

    Add visibility to manipulators and selection to PointManipulator (#9)
    * add visibility property;
    
    * remove extra prints
    
    * update points example

--- examples/points_manipulator.py
+++ examples/points_manipulator.py
@@ -1,7 +1,15 @@
+"""Example of using the points manipulator to re-position points.
+
+Enter the point selection mode and click on a point to activate the manipulator.
+Click and drag the arms of the manipulator to move the point.
+When a point is selected, hold space to rotate the view without losing the selection.
+"""
+
 import numpy as np
 
 import napari
 from napari_threedee.manipulators import PointManipulator
+
 
 points_data = np.array(
     [
@@ -14,18 +22,7 @@ points_data = np.array(
 viewer = napari.Viewer(ndisplay=3)
 points_layer = viewer.add_points(points_data, size=5)
 
-shapes_layer = viewer.add_shapes(points_data, shape_type='path',  edge_color='magenta')
-viewer.layers.selection = [points_layer]
-
-def on_data_update(event):
-    shapes_layer.selected_data = {0}
-    shapes_layer.remove_selected()
-
-    new_data = points_layer.data
-    shapes_layer.add(points_layer.data, shape_type='path', edge_color='magenta')
-
-points_layer.events.data.connect(on_data_update)
-
+# add the point manipulator
 point_manipulator = PointManipulator(viewer, points_layer, translator_length=20)
 
 napari.run()
