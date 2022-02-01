3fbe174ed3405b6f263ed304f1d6a919717b1d46
Alister Burt <alisterburt@gmail.com>
2022-02-01 21:09:42 +0000

    drag name consistency

--- src/napari_threedee/manipulators/render_plane_manipulator.py
+++ src/napari_threedee/manipulators/render_plane_manipulator.py
@@ -89,13 +89,13 @@ class RenderPlaneManipulator(BaseManipulator):
         self._initial_plane_pos = np.asarray(self._layer.experimental_slicing_plane.position)
         self._initial_rot_mat = self.rot_mat.copy()
 
-    def _while_translator_drag(self, selected_translator: int, translation_vector: np.ndarray):
+    def _while_dragging_translator(self, selected_translator: int, translation_vector: np.ndarray):
         new_translation = self._initial_plane_pos + translation_vector
         self._layer.experimental_slicing_plane.position = np.squeeze(new_translation)
         self.centroid = np.asarray(self._layer.experimental_slicing_plane.position)
 
-    def _while_rotator_drag(self, selected_rotator: int, rotation_matrix: np.ndarray):
+    def _while_dragging_rotator(self, selected_rotator: int, rotation_matrix: np.ndarray):
         self._layer.experimental_slicing_plane.normal = self.rotator_normals[0]
 
-    def _drag_callback_cleanup(self):
+    def _post_drag(self):
         self._initial_plane_pos = None
