a3b3c2fb338fdfb0221c347f29fa8af91c128628
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-03-10 10:48:23 +0100

    add mesh lighting controls (#25)
    * add initial headlight
    
    * update get visual
    
    * implement ThreeDeeModel
    
    * fix spacing
    
    * remove redundant method
    
    * Update examples/mesh_headlight.py
    
    Co-authored-by: alisterburt <alisterburt@gmail.com>
    
    * account for layer transforms
    
    * fix dim ordering
    
    * add mesh lighting plugin
    
    Co-authored-by: alisterburt <alisterburt@gmail.com>

--- src/napari_threedee/_dock_widget.py
+++ src/napari_threedee/_dock_widget.py
@@ -4,6 +4,7 @@ see: https://napari.org/docs/dev/plugins/hook_specifications.html
 from napari_plugin_engine import napari_hook_implementation
 
 from .annotators.qt_plane_point_annotator import PlanePointAnnotatorWidget
+from .lighting.qt_lighting_control import LightingControlWidget
 from .manipulators.qt_manipulators import QtRenderPlaneManipulatorWidget, QtPointManipulatorWidget, QtLayerManipulatorWidget
 
 
@@ -14,5 +15,6 @@ def napari_experimental_provide_dock_widget():
         (QtRenderPlaneManipulatorWidget, {"name": "Render plane manipulator"}),
         (QtPointManipulatorWidget, {"name": "Point manipulator"}),
         (QtLayerManipulatorWidget, {"name": "Layer manipulator"}),
-        (PlanePointAnnotatorWidget, {"name": "Point on plane annotator"})
+        (PlanePointAnnotatorWidget, {"name": "Point on plane annotator"}),
+        (LightingControlWidget, {"name": "Mesh lighting control"})
     ]
--- src/napari_threedee/utils/napari_utils.py
+++ src/napari_threedee/utils/napari_utils.py
@@ -34,7 +34,7 @@ def get_napari_visual(viewer, layer):
     visual
         The napari visual class for the layer.
     """
-    visual = viewer.window.qt_viewer.layer_to_visual[layer]
+    visual = viewer.window._qt_window._qt_viewer.layer_to_visual[layer]
 
     return visual
 
