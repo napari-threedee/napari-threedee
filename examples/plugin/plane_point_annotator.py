0e87f2b155cfdd408bf3406966b432a0fa2126f0
alisterburt <alisterburt@gmail.com>
2022-08-26 10:08:53 +0100

    nd-ify plane point annotator

--- examples/plugin/plane_point_annotator.py
+++ examples/plugin/plane_point_annotator.py
@@ -6,21 +6,19 @@ viewer = napari.Viewer(ndisplay=3)
 blobs = skimage.data.binary_blobs(
     length=64,
     volume_fraction=0.1,
-    n_dim=3
+    n_dim=4
 ).astype(float)
 
 plane_parameters_z = {
     'position': (32, 32, 32),
     'normal': (1, 0, 0),
     'thickness': 10,
-    'enabled': True
 }
 
 plane_parameters_y = {
     'position': (32, 32, 32),
     'normal': (0, 1, 0),
     'thickness': 10,
-    'enabled': True
 }
 
 viewer.add_image(
@@ -30,7 +28,8 @@ viewer.add_image(
     colormap='bop orange',
     blending='additive',
     opacity=0.5,
-    experimental_slicing_plane=plane_parameters_z)
+    depiction='plane',
+    plane=plane_parameters_z)
 
 viewer.add_image(
     blobs,
@@ -39,9 +38,10 @@ viewer.add_image(
     colormap='bop blue',
     blending='additive',
     opacity=0.5,
-    experimental_slicing_plane=plane_parameters_y)
+    depiction='plane',
+    plane=plane_parameters_y)
 
-viewer.add_points([], ndim=3, face_color='cornflowerblue')
+viewer.add_points([], ndim=4, face_color='cornflowerblue')
 
 viewer.window.add_plugin_dock_widget(
     plugin_name="napari-threedee", widget_name="point on plane annotator"
--- src/napari_threedee/mouse_callbacks.py
+++ src/napari_threedee/mouse_callbacks.py
@@ -83,21 +83,21 @@ def add_point_on_plane(
     if not plane_layer.visible:
         return
 
-    # Ensure added points will be visible until plane depth is sorted
-    points_layer.blending = 'translucent_no_depth'
-
-    # Calculate intersection of click with plane through data in data coordinates
-    intersection = plane_layer.experimental_slicing_plane.intersect_with_line(
-        line_position=viewer.cursor.position, line_direction=viewer.cursor._view_direction
+    # Calculate intersection of click with plane through data in displayed data (scene) coordinates
+    displayed_dims = np.asarray(viewer.dims.displayed)[list(viewer.dims.displayed_order)]
+    cursor_position_3d = np.asarray(viewer.cursor.position)[displayed_dims]
+    intersection_3d = plane_layer.plane.intersect_with_line(
+        line_position=cursor_position_3d, line_direction=viewer.cursor._view_direction[displayed_dims]
     )
+    intersection_nd = np.asarray(viewer.dims.point)
+    intersection_nd[displayed_dims] = intersection_3d
 
     # Check if click was on plane by checking if intersection occurs within
     # data bounding box. If not, exit early.
-    if not point_in_bounding_box(intersection, plane_layer.extent.data):
+    if not point_in_bounding_box(intersection_nd, plane_layer.extent.data):
         return
 
     if append:
-        points_layer.add(intersection)
+        points_layer.add(intersection_nd)
     else:
-        points_layer.data = intersection
-        # points_layer.add(intersection)
+        points_layer.data = intersection_nd
