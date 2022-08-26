51e418d8ffb12a26db4aea4f7bd8c6c65cafaaa3
alisterburt <alisterburt@gmail.com>
2022-08-26 15:31:34 +0100

    add 4d test

--- src/napari_threedee/_tests/test_mouse_callbacks.py
+++ src/napari_threedee/_tests/test_mouse_callbacks.py
@@ -21,3 +21,23 @@ def test_add_point_on_plane_3d(viewer_with_plane_and_points_3d):
     )
     assert len(points_layer.data) == 1
     np.testing.assert_array_almost_equal(points_layer.data[0], (14, 14, 14))
+
+
+def test_add_point_on_plane_4d(viewer_with_plane_and_points_4d):
+    viewer = viewer_with_plane_and_points_4d
+    points_layer = viewer.layers['Points']
+    plane_layer = viewer.layers['blobs_4d']
+
+    class DummyMouseEvent:
+        position = (0, 14, 14, 14)
+        _view_direction = np.array((0, 1, 0, 0))
+        modifiers = ['Alt']
+
+    add_point_on_plane(
+        viewer=viewer_with_plane_and_points_4d,
+        event=DummyMouseEvent,
+        points_layer=points_layer,
+        plane_layer=plane_layer,
+    )
+    assert len(points_layer.data) == 1
+    np.testing.assert_array_almost_equal(points_layer.data[0], (14, 14, 14, 14))
