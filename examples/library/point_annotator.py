5afd22ed5d2fbf1fa2e505b2b31a39805c9cce71
alisterburt <alisterburt@gmail.com>
2022-12-13 11:34:23 +0000

    examples

--- examples/library/point_annotator.py
+++ examples/library/point_annotator.py
@@ -4,17 +4,13 @@ import napari
 from skimage import data
 
 viewer = napari.Viewer(ndisplay=3)
-blobs = data.binary_blobs(
-    length=64, volume_fraction=0.1, n_dim=3
-).astype(float)
+blobs = data.binary_blobs(length=64, volume_fraction=0.1, n_dim=3).astype(float)
 
 # add a volume and render as plane
-# plane should be in 'additive' blending mode or depth looks all wrong
 plane_parameters = {
     'position': (32, 32, 32),
     'normal': (1, 0, 0),
     'thickness': 10,
-    'enabled': True
 }
 
 plane_layer = viewer.add_image(
@@ -24,13 +20,21 @@ plane_layer = viewer.add_image(
     colormap='bop orange',
     blending='translucent',
     opacity=0.5,
-    experimental_slicing_plane=plane_parameters
+    depiction='plane',
+    plane=plane_parameters,
 )
 
-points_layer = viewer.add_points([], size=5, face_color='cornflowerblue', ndim=3)
+points_layer = viewer.add_points(
+    data=[],
+    size=5,
+    face_color='cornflowerblue',
+    ndim=3
+)
 
 annotator = PointAnnotator(
-    viewer=viewer, image_layer=plane_layer, points_layer=points_layer
+    viewer=viewer,
+    image_layer=plane_layer,
+    points_layer=points_layer
 )
 
 napari.run()
