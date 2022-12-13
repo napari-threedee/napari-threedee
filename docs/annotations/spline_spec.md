cd96c0525335e26b1e79f601d0baedd5662c7af5
alisterburt <alisterburt@gmail.com>
2022-12-13 18:45:08 +0000

    io plugin working for spheres and splines

--- docs/annotations/spline_spec.md
+++ docs/annotations/spline_spec.md
@@ -8,8 +8,6 @@ can contain multiple splines.
 - `Points.data` is an `(n, d)` array of n d-dimensional points.
 - `Points.features` is a table which will contain a column called 
   `spline_id`, an integer id for each point.
-- `Points.metadata` is a dictionary with the following key:value pairs
-    - `"spline_order": int` the order of the splines.
 
 ## Zarr Array Specification
 The following assumes an `n3d` file has been read into a variable called 
@@ -26,6 +24,5 @@ zarr.load('spline.n3d')
 - `n3d.attrs["annotation_type"]` is `"spline"`
 - `n3d.attrs["spline_id"]` is an `(n, )` array containing the spline id of 
   each point
-- `n3d.attrs["spline_order"]` is the order of the splines as an integer
 
 
--- src/napari_threedee/annotators/io/spline.py
+++ src/napari_threedee/annotators/io/spline.py
@@ -1,3 +1,5 @@
+import os
+
 import napari
 from napari.types import LayerDataTuple
 import numpy as np
@@ -26,24 +28,22 @@ def validate_spline_zarr(n3d_zarr: zarr.Array):
         raise ValueError("cannot read as n3d spline.")
     elif SplineAnnotator.SPLINE_ID_FEATURES_KEY not in n3d_zarr.attrs:
         raise ValueError(f"{SplineAnnotator.SPLINE_ID_FEATURES_KEY} not found.")
-    elif SplineAnnotator.SPLINES_KEY not in n3d_zarr.attrs:
-        raise ValueError(f"{SplineAnnotator.SPLINES_KEY} not found")
 
 
-def layer_to_n3d_zarr(layer: napari.layers.Points) -> zarr.Array:
+def layer_to_n3d_zarr(layer: napari.layers.Points, path: os.PathLike) -> zarr.Array:
     """Convert a napari points layer into an n3d zarr array."""
     validate_spline_layer(layer)
-    n3d_zarr = zarr.array(layer.data)
+    n3d_zarr = zarr.open_array(
+        store=path,
+        shape=layer.data.shape,
+        dtype=layer.data.dtype,
+        mode="w"
+    )
+    n3d_zarr[...] = layer.data
     n3d_zarr.attrs[ANNOTATION_TYPE_KEY] = SplineAnnotator.ANNOTATION_TYPE
 
-    # get spline id per point from layer features
     spline_id_key = SplineAnnotator.SPLINE_ID_FEATURES_KEY
     n3d_zarr.attrs[spline_id_key] = list(layer.features[spline_id_key])
-
-    # get dict of spline t,c,k tuples from n3d metadata dict
-    n3d_metadata = layer.metadata[N3D_METADATA_KEY]
-    n3d_zarr.attrs[SplineAnnotator.SPLINES_KEY] = \
-        n3d_metadata[SplineAnnotator.SPLINES_KEY]
     return n3d_zarr
 
 
@@ -51,11 +51,10 @@ def n3d_zarr_to_layer_data_tuple(n3d_zarr: zarr.Array) -> LayerDataTuple:
     """Convert an n3d zarr array to an n3d spline points layer data tuple."""
     validate_spline_zarr(n3d_zarr)
     spline_id = n3d_zarr.attrs[SplineAnnotator.SPLINE_ID_FEATURES_KEY]
-    spline_order = n3d_zarr.attrs[SplineAnnotator.SPLINES_KEY]
     layer_kwargs = {
         "features": {SplineAnnotator.SPLINE_ID_FEATURES_KEY: spline_id},
-        "metadata": {
-            N3D_METADATA_KEY: {SplineAnnotator.SPLINES_KEY: spline_order},
-        }
+        "metadata": {N3D_METADATA_KEY: {
+            ANNOTATION_TYPE_KEY, SplineAnnotator.ANNOTATION_TYPE,
+        }}
     }
     return (np.array(n3d_zarr), layer_kwargs, "points")
