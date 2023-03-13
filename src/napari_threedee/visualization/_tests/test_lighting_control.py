dc41e855ec41606bdd748cc421383e7b7d2af113
Ashley Anderson <aganders3@gmail.com>
2023-03-13 11:15:49 -0400

    Use make_napari_viewer fixture in relevant tests

--- src/napari_threedee/visualization/_tests/test_lighting_control.py
+++ src/napari_threedee/visualization/_tests/test_lighting_control.py
@@ -1,21 +1,25 @@
-import napari
 import numpy as np
+import pytest
 
 from napari_threedee.visualization.lighting_control import LightingControl
 from napari_threedee.utils.napari_utils import get_napari_visual
 
 
-def test_lighting_control_enable():
-    viewer = napari.Viewer()
-    viewer.dims.ndisplay = 3
-
+@pytest.fixture
+def lone_triangle():
     vertices = np.array([
         [-1, 0, 0],
         [1, 0, 0],
         [0, 1, 0],
     ])
     faces = np.array([[0, 1, 2]], dtype=np.int16)
-    layer = viewer.add_surface((vertices, faces))
+    return (vertices, faces)
+
+
+def test_lighting_control_enable(make_napari_viewer, lone_triangle):
+    viewer = make_napari_viewer()
+    viewer.dims.ndisplay = 3
+    layer = viewer.add_surface(lone_triangle)
 
     lightting_control = LightingControl(viewer)
 
@@ -26,17 +30,10 @@ def test_lighting_control_enable():
     assert lightting_control.enabled
 
 
-def test_light_dir_unchanged_when_disabled():
-    viewer = napari.Viewer()
+def test_light_dir_unchanged_when_disabled(make_napari_viewer, lone_triangle):
+    viewer = make_napari_viewer()
     viewer.dims.ndisplay = 3
-
-    vertices = np.array([
-        [-1, 0, 0],
-        [1, 0, 0],
-        [0, 1, 0],
-    ])
-    faces = np.array([[0, 1, 2]], dtype=np.int16)
-    layer = viewer.add_surface((vertices, faces))
+    layer = viewer.add_surface(lone_triangle)
     visual = get_napari_visual(viewer=viewer, layer=layer)
 
     lightting_control = LightingControl(viewer)
@@ -50,17 +47,10 @@ def test_light_dir_unchanged_when_disabled():
     assert visual.node.shading_filter.light_dir == inital_light_dir
 
 
-def test_light_dir_changed_when_enabled():
-    viewer = napari.Viewer()
+def test_light_dir_changed_when_enabled(make_napari_viewer, lone_triangle):
+    viewer = make_napari_viewer()
     viewer.dims.ndisplay = 3
-
-    vertices = np.array([
-        [-1, 0, 0],
-        [1, 0, 0],
-        [0, 1, 0],
-    ])
-    faces = np.array([[0, 1, 2]], dtype=np.int16)
-    layer = viewer.add_surface((vertices, faces))
+    layer = viewer.add_surface(lone_triangle)
     visual = get_napari_visual(viewer=viewer, layer=layer)
 
     lightting_control = LightingControl(viewer)
