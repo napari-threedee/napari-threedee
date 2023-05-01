4fe569205bbc8f167bea46f125e891b29e364023
alisterburt <alisterburt@gmail.com>
2023-05-01 21:10:25 +0100

    lint

--- src/napari_threedee/data_models/_tests/test_spheres.py
+++ src/napari_threedee/data_models/_tests/test_spheres.py
@@ -4,4 +4,4 @@ import napari_threedee as n3d
 def test_empty_spheres():
     spheres = n3d.data_models.N3dSpheres(centers=[], radii=[])
     assert spheres.centers.shape == (0, 3)
-    assert spheres.radii.shape == (0, )
+    assert spheres.radii.shape == (0,)
