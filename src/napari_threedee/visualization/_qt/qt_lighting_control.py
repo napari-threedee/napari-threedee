f161256df9b9edfe91587feee11f7fb657531feb
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2023-05-15 08:24:24 +0200

    split lighting and ao

--- src/napari_threedee/visualization/_qt/qt_lighting_control.py
+++ src/napari_threedee/visualization/_qt/qt_lighting_control.py
@@ -1,8 +1,10 @@
 import igl
+from typing import List
 
 import napari
 from napari.qt.threading import FunctionWorker, thread_worker
-from magicgui import magicgui, widgets
+from napari.utils import progress
+from magicgui import magicgui, widgets, magic_factory
 from qtpy.QtWidgets import QPushButton, QWidget, QVBoxLayout, QCheckBox
 
 from napari_threedee.visualization.lighting_control import LightingControl
@@ -36,11 +38,6 @@ class QtLightingControlWidget(QWidget):
             self._layer_selection_widget.reset_choices
         )
 
-        # self._ambient_occlusion_checkbox = QCheckBox("ambient occlusion")
-        # self._ambient_occlusion_checkbox.setChecked(self.model.ambient_occlusion)
-        # self._ambient_occlusion_checkbox.clicked.connect(self._on_ambient_occlusion_clicked)
-        self._ambient_occlusion_widget = magicgui(self._set_ambient_occlusion, pbar={'visible': False, 'max': 0, 'label': 'working...'})
-
         # create set lighing widget
         self._lighting_button = QPushButton(self._ENABLE_TEXT, self)
         self._lighting_button.setChecked(False)
@@ -49,8 +46,6 @@ class QtLightingControlWidget(QWidget):
 
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(self._layer_selection_widget.native)
-        # self.layout().addWidget(self._ambient_occlusion_checkbox)
-        self.layout().addWidget(self._ambient_occlusion_widget.native)
         self.layout().addWidget(self._lighting_button)
 
     def _update_lighting_button(self):
@@ -76,13 +71,109 @@ class QtLightingControlWidget(QWidget):
     def _get_layers(self, widget):
         return [layer for layer in self._viewer.layers if isinstance(layer, napari.layers.Surface)]
 
-    def _set_ambient_occlusion(self, pbar: widgets.ProgressBar,  ambient_occlusion: bool=False) -> FunctionWorker[None]:
 
-        vertices, faces, values = self._viewer.layers[0].data
+class QtAmbientOcclusionWidget(QWidget):
 
-        @thread_worker(connect={'returned': pbar.hide})
-        def _set_ambient_occlusion():
-            vertex_normals = igl.per_vertex_normals(vertices, faces)
-            ao = igl.ambient_occlusion(vertices, faces, vertices, vertex_normals, 20)
-        pbar.show()
-        return _set_ambient_occlusion()
+    def __init__(self, viewer: napari.Viewer):
+        super().__init__()
+        self._viewer = viewer
+
+        # initialize state variables
+        self.selected_layers = []
+
+        # layers currently with ambient occlusion activated
+        self.current_ao_layers = []
+
+        # store the original values so they can be reset
+        self.original_data = {}
+
+        # create layer selection widget
+        self._layer_selection_widget = magicgui(
+            self.set_layers,
+            layers={
+                'widget_type': 'Select',
+                'choices': self._get_layers
+            },
+            auto_call=True
+        )
+
+        self._viewer.layers.events.inserted.connect(
+            self._layer_selection_widget.reset_choices
+        )
+        self._viewer.layers.events.removed.connect(
+            self._layer_selection_widget.reset_choices
+        )
+
+        self._ambient_occlusion_widget = magicgui(
+            self._set_ambient_occlusion,
+            call_button="update ambient occlusion",
+        )
+
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self._layer_selection_widget.native)
+        # self.layout().addWidget(self._ambient_occlusion_checkbox)
+        self.layout().addWidget(self._ambient_occlusion_widget.native)
+
+    def _get_layers(self, widget):
+        return [layer for layer in self._viewer.layers if isinstance(layer, napari.layers.Surface)]
+
+    def set_layers(self, layers: List[napari.layers.Surface]):
+        self.selected_layers = layers
+
+    # def _set_ambient_occlusion(self, pbar: widgets.ProgressBar) -> FunctionWorker[None]:
+    #
+    #     current_selection = set(self.selected_layers)
+    #     current_ao = set(self.current_ao_layers)
+    #
+    #     layers_to_add_ao_set = current_selection.difference(current_ao)
+    #     layers_to_add_ao = list(layers_to_add_ao_set)
+    #     layers_to_remove_ao = list(current_ao.difference(current_selection))
+    #     self.current_ao_layers = list(layers_to_add_ao_set.union(current_ao.intersection(current_selection)))
+    #
+    #     @thread_worker(connect={'returned': pbar.hide})
+    #     def _set_ambient_occlusion():
+    #         for layer in layers_to_add_ao:
+    #             vertices, faces, values = layer.data
+    #             vertex_normals = igl.per_vertex_normals(vertices, faces)
+    #             ao = igl.ambient_occlusion(vertices, faces, vertices, vertex_normals, 20)
+    #             attenuation_factors = 1 - ao
+    #             attenuated_values = attenuation_factors * values
+    #
+    #             layer.data = (vertices, faces, attenuated_values)
+    #
+    #             self.original_data.update({layer: (vertices, faces, values)})
+    #
+    #         for layer in layers_to_remove_ao:
+    #             # remove the ao
+    #             layer_data = self.original_data.pop(layer)
+    #             print(layer_data[2])
+    #             layer.data = layer_data
+    #
+    #     pbar.show()
+    #     return _set_ambient_occlusion()
+    def _set_ambient_occlusion(self) -> None:
+        current_selection = set(self.selected_layers)
+        current_ao = set(self.current_ao_layers)
+
+        layers_to_add_ao_set = current_selection.difference(current_ao)
+        layers_to_add_ao = list(layers_to_add_ao_set)
+        layers_to_remove_ao = list(current_ao.difference(current_selection))
+        self.current_ao_layers = list(layers_to_add_ao_set.union(current_ao.intersection(current_selection)))
+
+        with progress(layers_to_add_ao) as adding_progress:
+            for layer in adding_progress:
+                vertices, faces, values = layer.data
+                vertex_normals = igl.per_vertex_normals(vertices, faces)
+                ao = igl.ambient_occlusion(vertices, faces, vertices, vertex_normals, 20)
+                attenuation_factors = 1 - ao
+                attenuated_values = attenuation_factors * values
+
+                layer.data = (vertices, faces, attenuated_values)
+
+                self.original_data.update({layer: (vertices, faces, values)})
+
+        with progress(layers_to_remove_ao) as removing_progress:
+            for layer in removing_progress:
+                # remove the ao
+                layer_data = self.original_data.pop(layer)
+                layer.data = layer_data
