f41ca84705b5035ca1a0bae9e462e27588172afb
alisterburt <alisterburt@gmail.com>
2023-07-23 16:58:33 +0100

    add test for point in bounding box

--- src/napari_threedee/utils/_tests/test_geometry.py
+++ src/napari_threedee/utils/_tests/test_geometry.py
@@ -1,19 +1,24 @@
 import numpy as np
 import pytest
 
-from napari_threedee.utils.geometry import signed_angle_between_vectors, rotation_matrix_around_vector_3d
+from napari_threedee.utils.geometry import (
+    signed_angle_between_vectors,
+    rotation_matrix_around_vector_3d,
+    point_in_bounding_box,
+)
 
 
 @pytest.mark.parametrize(
     "vector_0,vector_1,rotation_axis,expected_angle",
     [
-        (np.array([0, 1, 1]), np.array([0, -1, 1]), np.array([1, 0, 0]), np.pi/2),
-        (np.array([0, 1, 1]), np.array([0, -1, 1]), np.array([-1, 0, 0]), -np.pi/2),
-        (np.array([0, 1, 1]), np.array([0, 1, -1]), np.array([1, 0, 0]), -np.pi/2),
-        (np.array([0, 1, 1]), np.array([0, 1, -1]), np.array([-1, 0, 0]), np.pi/2)
+        (np.array([0, 1, 1]), np.array([0, -1, 1]), np.array([1, 0, 0]), np.pi / 2),
+        (np.array([0, 1, 1]), np.array([0, -1, 1]), np.array([-1, 0, 0]), -np.pi / 2),
+        (np.array([0, 1, 1]), np.array([0, 1, -1]), np.array([1, 0, 0]), -np.pi / 2),
+        (np.array([0, 1, 1]), np.array([0, 1, -1]), np.array([-1, 0, 0]), np.pi / 2)
     ]
 )
-def test_signed_angle_between_vectors(vector_0, vector_1, rotation_axis, expected_angle):
+def test_signed_angle_between_vectors(vector_0, vector_1, rotation_axis,
+                                      expected_angle):
     angle = signed_angle_between_vectors(
         vector_0=vector_0,
         vector_1=vector_1,
@@ -25,7 +30,8 @@ def test_signed_angle_between_vectors(vector_0, vector_1, rotation_axis, expecte
 
 def test_rotation_matrix_around_vector_3d():
     angle = np.pi / 2
-    rotation_matrix = rotation_matrix_around_vector_3d(angle=angle, vector=np.array([1, 0, 0]))
+    rotation_matrix = rotation_matrix_around_vector_3d(angle=angle,
+                                                       vector=np.array([1, 0, 0]))
 
     expected_matrix = np.array(
         [
@@ -35,3 +41,16 @@ def test_rotation_matrix_around_vector_3d():
         ]
     )
     np.testing.assert_allclose(rotation_matrix, expected_matrix)
+
+
+def test_point_in_bounding_box():
+    bbox = np.array([[0, 0, 0], [1, 1, 1]])
+
+    # point inside
+    assert point_in_bounding_box(point=np.array([0.5, 0.5, 0.5]), bounding_box=bbox)
+
+    # point outside
+    assert not point_in_bounding_box(point=np.array([1.5, 1.5, 1.5]), bounding_box=bbox)
+
+    # point on edge should be inside
+    assert point_in_bounding_box(point=np.array([1, 1, 1]), bounding_box=bbox)
