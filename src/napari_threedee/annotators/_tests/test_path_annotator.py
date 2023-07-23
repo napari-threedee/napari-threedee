ca571505102dd198ed1e08655f3108323d8ed998
alisterburt <alisterburt@gmail.com>
2023-07-23 16:42:15 +0100

    add test for draw_paths

--- src/napari_threedee/annotators/_tests/test_path_annotator.py
+++ src/napari_threedee/annotators/_tests/test_path_annotator.py
@@ -1,10 +1,11 @@
+import numpy as np
 from napari.layers import Points
 
 import napari_threedee.annotators.paths.constants
 from napari_threedee.annotators.paths import PathAnnotator
 
 
-def test_spline_annotator_instantiation(make_napari_viewer, blobs_layer_4d_plane):
+def test_path_annotator_instantiation(make_napari_viewer, blobs_layer_4d_plane):
     viewer = make_napari_viewer(ndisplay=3)
     plane_layer = viewer.add_layer(blobs_layer_4d_plane)
     annotator = PathAnnotator(
@@ -16,8 +17,8 @@ def test_spline_annotator_instantiation(make_napari_viewer, blobs_layer_4d_plane
     assert len(annotator.points_layer.data) == 0
 
 
-def test_add_point(spline_annotator):
-    points_layer = spline_annotator.points_layer
+def test_add_point(path_annotator):
+    points_layer = path_annotator.points_layer
     label = napari_threedee.annotators.paths.constants.PATH_ID_FEATURES_KEY
 
     # start empty
@@ -31,26 +32,52 @@ def test_add_point(spline_annotator):
     # change filamanet_id in annotator, add point, check matches
     points_layer.add([2, 3, 4, 5])
     assert points_layer.features[label][1] == 0
-    assert spline_annotator.active_path_id == 0
+    assert path_annotator.active_path_id == 0
 
     # create new path, make sure id advanced
-    spline_annotator.activate_new_path_mode()
+    path_annotator.activate_new_path_mode()
     points_layer.add([2, 3, 4, 5])
     assert points_layer.features[label][2] == 1
-    assert spline_annotator.active_path_id == 1
+    assert path_annotator.active_path_id == 1
 
 
-def test_get_colors(spline_annotator):
+def test_get_colors(path_annotator):
     """Test getting spline colors from the annotator."""
     # add a point. the first spline gets id 0
-    points_layer = spline_annotator.points_layer
+    points_layer = path_annotator.points_layer
     points_layer.add([1, 2, 3, 4])
 
     # create a new spline, this advances the spline id to 1
-    spline_annotator.activate_new_path_mode()
+    path_annotator.activate_new_path_mode()
     points_layer.add([2, 3, 4, 5])
 
-    spline_colors = spline_annotator._get_path_colors()
+    spline_colors = path_annotator._get_path_colors()
     for spline_id in (0, 1):
         # check that both spline ids are in the colors dict
         assert spline_id in spline_colors
+
+
+def test_draw_paths(path_annotator):
+    """Test drawing paths adds paths with different colors to the viewer."""
+    # add a first path to the empty points layer
+    points_layer = path_annotator.points_layer
+    points_layer.add([1, 2, 3, 4])
+    points_layer.add([2, 3, 4, 5])
+
+    # add a second path
+    path_annotator.activate_new_path_mode()
+    points_layer.add([3, 4, 5, 6])
+    points_layer.add([4, 5, 6, 7])
+
+    # add an invalid path with only one point
+    path_annotator.activate_new_path_mode()
+    points_layer.add([5, 6, 7, 8])
+
+    # explicitly draw paths and check content of shapes layer
+    path_annotator._draw_paths()
+    shapes_layer = path_annotator.shapes_layer
+    assert len(shapes_layer.data) == 2
+    assert len(shapes_layer.edge_color) == 2
+
+    # check two paths have different colors
+    assert not np.allclose(shapes_layer.edge_color[0], shapes_layer.edge_color[1])
