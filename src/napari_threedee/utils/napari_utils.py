ccde7f182c899217a12f62339fd4f9b47916cedc
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2024-04-10 18:31:50 +0200

    add note about upcoming deprecation

--- src/napari_threedee/utils/napari_utils.py
+++ src/napari_threedee/utils/napari_utils.py
@@ -56,7 +56,7 @@ def get_vispy_root_node(viewer: napari.Viewer, layer):
     This is the node that layers are added to.
     """
     # this will need to be updated in napari 0.5.0
-    viewer.window._qt_window._qt_viewer.canvas.view.scene
+    # viewer.window._qt_window._qt_viewer.canvas.view.scene
     qt_viewer = viewer.window._qt_window._qt_viewer
     return qt_viewer.view.scene
 
