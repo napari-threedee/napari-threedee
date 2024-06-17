5967589c8e0a5208ea46465a643a0b2fed3fff88
Peter Sobolewski <pete.sd@gmail.com>
2024-06-16 21:39:05 -0400

    Deactivate button and change text in 2D

--- src/napari_threedee/_backend/threedee_widget_base.py
+++ src/napari_threedee/_backend/threedee_widget_base.py
@@ -33,13 +33,14 @@ class QtThreeDeeWidgetBase(QWidget):
         self.layout().addWidget(self.layer_selection_widget)
         self.layout().addWidget(self.activate_button)
 
-        self.viewer.dims.events.ndisplay.connect(self._on_ndisplay_change)
-        if viewer.dims.ndisplay == 2:
-            show_info("Viewer needs to be in 3D mode.")
+        # Don't allow activating in 2D mode or with <3D data
+        if self.viewer.dims.ndisplay == 2 or self.viewer.dims.ndim < 3:
+            show_info("Viewer needs to be in 3D mode with 3D+ data.")
             self.activate_button.setEnabled(False)
-            self.activate_button.setText('2D, disabled')
+            self.activate_button.setText('2D data/viewer, disabled')
         
         self.activate_button.clicked.connect(self.on_activate_button_click)
+        self.viewer.dims.events.ndisplay.connect(self._on_ndisplay_change)
 
     @property
     def layer_selection_widget(self) -> QWidget:
@@ -51,13 +52,29 @@ class QtThreeDeeWidgetBase(QWidget):
             self.activate_button.setText('deactivate')
         else:
             self.model.enabled = False
-            self.activate_button.setText('activate')
+            # permit deactivating in 2D mode, but don't permit reactivation
+            if self.viewer.dims.ndisplay == 2 or self.viewer.dims.ndim < 3:
+                self.activate_button.setEnabled(False)
+                self.activate_button.setText('2D data/viewer, disabled')
+            else:
+                self.activate_button.setText('activate')
 
     def _on_ndisplay_change(self, event):
         new_ndisplay = event.value
-        if new_ndisplay == 2:
+        # going to 2D and was never activated
+        if (new_ndisplay == 2 or self.viewer.dims.ndim < 3) and self.activate_button.text() != 'deactivate':
             self.activate_button.setEnabled(False)
-            self.activate_button.setText('2D, disabled')
-        else:
-            self.activate_button.setEnabled(True)
-            self.activate_button.setText('activate')
+            self.activate_button.setText('2D data/viewer, disabled')
+        # going to 3D with 3D+ data
+        elif self.viewer.dims.ndim > 2:
+            # if never activated, enable activation
+            if self.activate_button.text() == '2D data/viewer, disabled':
+                self.activate_button.setChecked(False)
+                self.activate_button.setEnabled(True)
+                self.activate_button.setText('activate')
+                self.model.enabled = False
+            # otherwise assume was activated, so enable deactivation
+            else:
+                self.activate_button.setEnabled(True)
+                self.activate_button.setChecked(True)
+                self.activate_button.setText('deactivate')
