2b38491b0c9da98820c11c2c965d07fb892a1bcd
Aysecan Unal <aysecan.uenal@h-its.org>
2024-07-09 19:18:09 +0200

    Start of dipole annotator

--- src/napari_threedee/data_models/__init__.py
+++ src/napari_threedee/data_models/__init__.py
@@ -2,3 +2,4 @@ from .points import N3dPoints
 from .paths import N3dPath, N3dPaths
 from .spheres import N3dSpheres
 from .surfaces import N3dSurface, N3dSurfaces
+from .dipoles import N3dDipole, N3dDipoles
