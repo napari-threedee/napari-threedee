271f4b23f644cd690c6401737374728a6dd72a45
aysecanuenal <150450168+aysecanuenal@users.noreply.github.com>
2024-10-02 17:13:21 +0200

    Corrections for dipoles (#257)
    * Corrections for dipoles
    
    * Corrections for dipoles

--- docs/examples/library/dipole_annotator_library.py
+++ docs/examples/library/dipole_annotator_library.py
@@ -1,8 +1,8 @@
 """
-Sphere annotator (library)
+Dipole annotator (library)
 ==================================
 
-An example controlling the sphere annotator,
+An example controlling the dipole annotator,
 using napari-threedee as a library.
 """
 import napari
